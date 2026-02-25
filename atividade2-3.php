47bafece3079f611d09933940dfaf88d46db3e06
LuisMiguelDaSilvaPacheco <lm.luismiguel10@gmail.com>
2026-02-25 14:03:32 -0300

    Fix leap year condition in atividade2-3.php

--- atividade2-3.php
+++ atividade2-3.php
@@ -4,7 +4,7 @@ print "Os anos bissextos entre 1980 e 2024 são: \n";
 
 for($ano = 1980; $ano <= 2024; $ano++){
 
- if($ano % 4 == 0  and $ano % 100 != 0 or $ano % 4 == 0  and $ano % 400 == 0 ){
+ if(($ano % 4 == 0  and $ano % 100 != 0) or ($ano % 4 == 0  and $ano % 400 == 0) ){
 
   print "$ano \n";
 
